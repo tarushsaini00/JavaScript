a40dacb3e3864ead78004903786c2df680fa48fe
Tarush Saini <90197321+tarushsaini00@users.noreply.github.com>
2023-09-03 19:07:14 +0000

    datatype

--- basics/02_Datatype.js
+++ basics/02_Datatype.js
@@ -1,4 +1,25 @@
 "use strict"; // treat all JS code as newer version
 
-// alert("hello")
+// alert("hello") //we are using nodejs, not browser
+
+
+let name = "Tarush"
+let age  = 55
+let isLoogedIn = true
+
+
+// number => 2 to power 53
+// bigint => 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);      // object
+
 
