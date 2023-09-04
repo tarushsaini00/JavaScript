f42b869e34cfe61a391df346b902d3cce974a468
Tarush Saini <90197321+tarushsaini00@users.noreply.github.com>
2023-09-04 13:52:06 +0000

    conversion_functions

---
+++ basics/03_conversionfunction.js
@@ -0,0 +1,24 @@
+let score = "false"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// const {score} = req.body
+
+// "33" => 33
+// "33abc" => NaN
+// True => 1; false => 0
+
+let isLoogedIn = ""
+
+let booleanIsLoogedIn = Boolean(isLoogedIn)
+
+console.log(booleanIsLoogedIn);
+
+// 1 => True; 0 => false
+//"" => false
+// "krishna" => true
+
