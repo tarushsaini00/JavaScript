fb3922644cd76313c4725b5e75f374d9ac22ada5
Tarush Saini <90197321+tarushsaini00@users.noreply.github.com>
2023-09-24 12:53:52 +0000

    commit

---
+++ 3_basics/functions.js
@@ -0,0 +1,15 @@
+
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("D");
+    console.log("R"); 
+}
+
+// sayMyName()
+
+function addTowNumbers (number1, number2){
+    console.log(number1 + number2);
+}
+
+const result = addTowNumbers(3, 5);
