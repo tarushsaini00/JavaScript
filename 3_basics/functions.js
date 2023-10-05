2ef389f4ee5ae43312d07ad55c5f28b5da613def
Tarush Saini <90197321+tarushsaini00@users.noreply.github.com>
2023-10-05 16:47:18 +0000

    commit

--- 3_basics/functions.js
+++ 3_basics/functions.js
@@ -23,9 +23,47 @@ const result = addTowNumbers(3, 5);
 // console.log("Result: ", result);
 
 function loginUserMessage(username){
-    return`${username} just logged in`
+    if(username === undefined){
+        console.log("Please Enter a username")
+        return;
+    }
+    return `${username} just logged in`
 }
 
-console.log(loginUserMessage(""));
+// console.log(loginUserMessage(""));
 // console.log(loginUserMessage("Kushboo"));
 // console.log(loginUserMessage());
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1;
+}
+
+console.log(calculateCartPrice(200, 500, 1045, 45666));
+
+const user = {
+    username: "Modiji",
+    price: 199
+}
+
+function handelObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handelObject(user)
+
+handelObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [100, 200, 300, 1000]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([100, 200, 300, 1000]));
+
